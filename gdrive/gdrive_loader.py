fd6e231e946b7a176b21a17526d524ed6b2fc88a
Minghao1999 <sun989minghao@gmail.com>
2026-03-16 13:19:32 -0400

    load data

--- gdrive/gdrive_loader.py
+++ gdrive/gdrive_loader.py
@@ -1,11 +1,15 @@
 import requests
 import os
+from datetime import date
 
 def download_file(file_id, output):
 
     url = f"https://docs.google.com/spreadsheets/d/{file_id}/export?format=xlsx"
 
     r = requests.get(url)
+
+    if r.status_code != 200:
+        raise Exception("Download failed")
 
     with open(output, "wb") as f:
         f.write(r.content)
@@ -16,13 +20,18 @@ def download_daily_files():
     inventory_id = "1Upbjv3sonyg40F9Rk1wTp7IltCK4ROB-"
     empty_id = "1NV_g66yMuLzttr2X7fvx71oMjTpPlQ2d"
 
-    # 创建 gdrive 文件夹（如果不存在）
-    os.makedirs("gdrive", exist_ok=True)
+    os.makedirs("gdrive/data", exist_ok=True)
 
-    inventory_path = os.path.join("gdrive", "inventory.xlsx")
-    empty_path = os.path.join("gdrive", "empty.xlsx")
+    today = date.today().strftime("%Y-%m-%d")
 
-    download_file(inventory_id, inventory_path)
-    download_file(empty_id, empty_path)
+    inventory_path = f"gdrive/data/inventory_{today}.xlsx"
+    empty_path = f"gdrive/data/empty_{today}.xlsx"
+
+    # 如果今天文件不存在才下载
+    if not os.path.exists(inventory_path):
+        download_file(inventory_id, inventory_path)
+
+    if not os.path.exists(empty_path):
+        download_file(empty_id, empty_path)
 
     return inventory_path, empty_path
